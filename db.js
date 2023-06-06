18fe8bbacc6774aad06fa84594b23717b4f21086
Ron v. Milman <milmanvron@gmail.com>
2023-06-06 13:04:06 +0300

    cleanup mistakingly tracked files

--- db.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-fs.appendFileSync("./shuman.txt", "testestest");
-const data = fs.readFileSync("./shuman.txt", "utf-8");
-console.log(data);
-
-module.exports = {
-  data,
-};
--- dockerfile
+++
@@ -1,6 +0,0 @@
-from alpine
-run apk add --update nodejs npm
-workdir /app
-copy . .
-cmd ["npm", "start"]
-expose 3000
